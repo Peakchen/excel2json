d9393a71738665ddebe67975b3b452655fc74398
stefan <2572915286@qq.com>
2020-08-11 17:51:36 +0800

    fix export bat.

--- export_for_windows.bat
+++ export_for_windows.bat
@@ -1,8 +1,19 @@
-@echo on
+@echo off
 
+set findpip=0
+
+@for /f "tokens=1" %%i in ('pip --version ^| findstr /C:"pip"') do ^
+if %%i == pip set findpip=1
+
+if  %findpip% == 1 (
+	python -m pip --version
+) else (
+	echo "need download pip"
+	rem python downpip.py
+	python -m pip install -U pip
+)
+  
 pip install xlrd
 python exls2file.py
-
-@echo off
 
 pause
